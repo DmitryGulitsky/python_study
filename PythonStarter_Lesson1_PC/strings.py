abf670ac8580540cc9924ac902f965b4378d859b
Dmitry Gulitsky <dmitry.gulitsky@gmail.com>
2019-06-27 18:30:24 +0300

    Add strings.py. Use string type operators

---
+++ PythonStarter_Lesson1_PC/strings.py
@@ -0,0 +1,12 @@
+spam = 'abcdef'
+
+print('spam == ', spam)
+print('len(spam) == ', len(spam))
+print('spam[5] == ', spam[5])
+print('len(spam) - 1 == ', len(spam) - 1)
+print('spam[-2] == ', spam[-2])
+print('spam[2:5] == ', spam[2:5])
+print('spam[:4] == ', spam[:4])
+print('spam[-2:] == ', spam[-2:])
+print('spam[:-2] == ', spam[:-2])
+print('spam[1:-2] == ', spam[1:-2])
